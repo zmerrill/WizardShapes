48130d373ea1b6c21059a5d099c07184908703d8
zmerrill <wizardshapes@gmail.com>
2013-10-08 00:26:21 -0500

    Level stuff

--- app/assets/javascripts/games/boilerplate-master/js/resources.js
+++ app/assets/javascripts/games/boilerplate-master/js/resources.js
@@ -5,10 +5,21 @@ game.resources = [
      */
     // our level tileset
     {name: "area01_level_tiles",  type:"image", src: "/assets/games/boilerplate-master/data/img/map/area01_level_tiles.png"},
+     // the main player spritesheet
+    {name: "gripe_run_right", type:"image", src: "/assets/games/boilerplate-master/data/img/sprite/gripe_run_right.png"},
+    // the spinning coin spritesheet
+    {name: "spinning_coin_gold",  type:"image", src: "/assets/games/boilerplate-master/data/img/sprite/spinning_coin_gold.png"},
+    // our enemty entity
+    {name: "wheelie_right",       type:"image", src: "/assets/games/boilerplate-master/data/img/sprite/wheelie_right.png"},
      
     /* 
      * Maps. 
      */
-    {name: "area01", type: "tmx", src: "/assets/games/boilerplate-master/data/map/area01.tmx"}
- 
+    {name: "area01", type: "tmx", src: "/assets/games/boilerplate-master/data/map/area01.tmx"},
+
+ 	/*
+ 	* Backgrounds
+ 	*/
+ 	{name: "area01_bkg0",         type:"image", src: "/assets/games/boilerplate-master/data/img/area01_bkg0.png"},
+    {name: "area01_bkg1",         type:"image", src: "/assets/games/boilerplate-master/data/img/area01_bkg1.png"}
 ];
