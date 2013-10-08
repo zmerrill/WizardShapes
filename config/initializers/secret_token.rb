7da63a17a9b0e739d6c6deb1cd8633e108dd9df8
zmerrill <wizardshapes@gmail.com>
2013-10-07 19:36:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WizardShapes::Application.config.secret_key_base = '7798e6143cebd8a6bc09e250d3122184d712c9143857cf2ecb04feef10269bc331bcaf5b527ee15be206f471912eccd59249e9f4ce795c6b3aea927a1ce85492'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WizardShapes::Application.config.session_store :cookie_store, key: '_wizard_shapes_session'
