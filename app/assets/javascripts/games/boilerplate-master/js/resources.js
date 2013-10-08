be00be8ddfe2be9c3e2803825ba99123bc91c3b1
zmerrill <wizardshapes@gmail.com>
2013-10-07 23:34:51 -0500

    cleaned up resources

--- app/assets/javascripts/games/boilerplate-master/js/resources.js
+++ app/assets/javascripts/games/boilerplate-master/js/resources.js
@@ -4,11 +4,11 @@ game.resources = [
      * Graphics.
      */
     // our level tileset
-    {name: "area01_level_tiles",  type:"image", src: "data/img/map/area01_level_tiles.png"},
+    {name: "area01_level_tiles",  type:"image", src: "/assets/games/boilerplate-master/data/img/map/area01_level_tiles.png"},
      
     /* 
      * Maps. 
      */
-    {name: "area01", type: "tmx", src: "data/map/area01.tmx"}
+    {name: "area01", type: "tmx", src: "/assets/games/boilerplate-master/data/map/area01.tmx"}
  
 ];
